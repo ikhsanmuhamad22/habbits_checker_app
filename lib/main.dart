ba99aca0db037c4d6c76ab17323fda3b9181a2aa
ikhsan <ikhsanmuhamad3885@gmail.com>
2025-12-29 20:44:12 +0700

    add: responsive widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Habit checker',
       theme: AppTheme.lightTheme,
       home: MainPage(),
     );
